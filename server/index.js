9e25bbc22bff2bc559f63f00b52b00759493c96a
Wilmar Osorio <wilmar.majano16@itca.edu.sv>
2019-03-31 15:36:58 -0600

    Configurando Express en el proyecto

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app=express();
+//Server Settings 
+
+//Middelwares
+
+//Routes
+
+//Starting the server
+app.listen(3000,()=>
+{
+    console.log("Server on port 3000");
+}
+);
