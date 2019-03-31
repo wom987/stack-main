668e698ff9ba56b31b0ea708e55c2d18924b2c39
Wilmar Osorio <wilmar.majano16@itca.edu.sv>
2019-03-31 15:58:37 -0600

    configuracion de nodemon y morgan

--- server/index.js
+++ server/index.js
@@ -1,13 +1,17 @@
 const express = require('express');
+const morgan = require('morgan');
 const app=express();
 //Server Settings 
-
+app.set('port',process.env.PORT|| 3000);
 //Middelwares
-
+//ver peticiones que se hacen al servidor
+app.use(morgan('dev'));
+//enteder archivos Json
+app.use(express.json());
 //Routes
 
 //Starting the server
-app.listen(3000,()=>
+app.listen(app.get('port'),()=>
 {
     console.log("Server on port 3000");
 }
