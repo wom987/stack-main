19b1588f04b0a6d39049c27f6a22189628941f21
Wilmar Osorio <wilmar.majano16@itca.edu.sv>
2019-04-01 21:16:15 -0600

    Modificacion de Services y configuracion basica de modelos

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeesComponent } from './components/employees/employees.component';
+//importaciones personalizadas
+import{FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,8 @@ import { EmployeesComponent } from './components/employees/employees.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
