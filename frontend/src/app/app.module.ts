a2e5882e70f9191a25f4bd4509dae205f62d0ea4
Wilmar Osorio <wilmar.majano16@itca.edu.sv>
2019-04-01 21:37:08 -0600

    se agrego httpClientModule y se conf user por defecto

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeesComponent } from './components/employees/employees.component';
 //importaciones personalizadas
 import{FormsModule} from '@angular/forms';
+import{HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -15,7 +15,8 @@ import{FormsModule} from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/models/employee.ts
+++ frontend/src/app/models/employee.ts
@@ -1,7 +1,17 @@
 export class Employee {
+
+    constructor(_id='', name='', position='', office='', salary=0){
+        
+        this._id=_id;
+        this.name= name;
+        this.position= position;
+        this.salary= salary;
+        this.office=office;
+
+    }
     _id:string;
     name:string;
     position:string;
     office:string;
-    salart:number;
+    salary:number;
 }
